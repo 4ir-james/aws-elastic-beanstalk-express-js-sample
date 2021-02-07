d294b67566c8c04da3aaed7b5d83d37d80fe0d9c
4ir-james <78667817+4ir-james@users.noreply.github.com>
2021-02-06 20:00:35 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hfasfello World!'));
+app.get('/', (req, res) => res.send('Burnand Rules!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
